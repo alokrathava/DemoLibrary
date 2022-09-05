62ee2bff9bf2dd31466f639b85e1cde37b7eca7f
Alok Rathava <45447327+alokrathava@users.noreply.github.com>
2022-09-05 00:11:38 -0400

    Update General.kt

--- app/mylibrary/src/main/java/com/agrcyberlabsindia/mylibrary/General.kt
+++ app/mylibrary/src/main/java/com/agrcyberlabsindia/mylibrary/General.kt
@@ -3,7 +3,7 @@ package com.agrcyberlabsindia.mylibrary
 import android.content.Context
 import android.widget.Toast
 
-class General {
+public class General {
 
     fun showMsg(msg: String, context: Context) {
         Toast.makeText(context, msg, Toast.LENGTH_LONG).show()
